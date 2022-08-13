7bda93d3876db995ebaa55eea48b99e94d897aa0
MrMernDeveloper <rakibkst16@gmail.com>
2022-08-13 17:42:30 +0600

    added some js or css

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,15 @@
-console.log('hello form js');
-console.log(document);
+const sections = document.querySelectorAll('section')
+for (const section of sections) {
+    
+    console.log(section);
+    section.style.border = '2px solid black';
+    section.style.borderRadius = '5px'
+    section.style.marginBottom = '15px'
+    section.style.paddingLeft = '10px'
+    section.style.backgroundColor= 'lightBlue'
+}
+// const places = document.getElementById('place');
+// places.style.backgroundColor ='yellow'
+const place = document.getElementById('place');
+place.classList.add('text-center');
+place.classList.remove('font');
